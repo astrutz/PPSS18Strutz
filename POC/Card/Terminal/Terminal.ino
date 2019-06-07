cf3293a056a4ec68f184fdad852c5106119dad48
Alexander Strutz <a.strutz@kernpunkt.de>
2019-06-07 17:05:07 +0200

    [PROTO] Built a working terminal with logoff button

--- POC/Card/Terminal/Terminal.ino
+++ POC/Card/Terminal/Terminal.ino
@@ -1,66 +1,129 @@
-/*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read data from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- * 
- * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- * 
- * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
- * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
- * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
- * keep the PICCs at reading distance until complete.
- * 
- * @license Released into the public domain.
- * 
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- */
-
 #include <SPI.h>
 #include <MFRC522.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include "ESP8266HTTPClient.h"
+#include "ESP8266WiFi.h"
 
-#define RST_PIN         D2          // Configurable, see typical pin layout above
-#define SS_PIN          D4         // Configurable, see typical pin layout above
+#define RST_PIN  0          // GPIO0 = D3
+#define SS_PIN   15         // GPIO15 = D8
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+const char* ssid = "kernarea.de/BYOD";
+const char* password =  "4m0b!l35";
+const byte interruptPin = 4; //GPIO4 = D2
+boolean pressed = false;
+
+MFRC522 rfid(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+MFRC522::MIFARE_Key key; 
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
 
 void setup() {
+  attachInterrupt(digitalPinToInterrupt(interruptPin), logOff, RISING);
 	Serial.begin(115200);		// Initialize serial communications with the PC
-	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  
 	SPI.begin();			// Init SPI bus
-	mfrc522.PCD_Init();		// Init MFRC522
-	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
-	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+	rfid.PCD_Init();		// Init MFRC522
+  delay(5);
+
+   for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println("Setup complete");
 }
 
 void loop() {
+
+  if (WiFi.status() == WL_CONNECTED && pressed == true) {
+
+      HTTPClient http;
+      http.begin("http://jiracardserver.herokuapp.com/logoff");
+
+      int httpCode = http.PUT("");
+
+      if (httpCode > 0) {
+        Serial.println(http.getString());
+      }
+      resetFunc();
+  }
+  pressed = false;
+  
 	// Look for new cards
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
-		return;
-	}
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
 
-	// Select one of the cards
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
-		return;
-	}
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
 
-	// Dump debug info about the card; PICC_HaltA() is automatically called
-	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+    sendUidToServer();
+  }
+  else Serial.println(F("Card read previously."));
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+
+}
+
+void sendUidToServer() {
+  unsigned long hex_num;
+  hex_num =  nuidPICC[0] << 24;
+  hex_num += nuidPICC[1] << 16;
+  hex_num += nuidPICC[2] <<  8;
+  hex_num += nuidPICC[3];
+
+  if (WiFi.status() == WL_CONNECTED) {
+
+      HTTPClient http;
+      String hexString = String(hex_num);
+      char url[128];
+      char buf[32];
+      hexString.toCharArray(buf, 32);
+      strcpy(url, "http://jiracardserver.herokuapp.com/login/");
+      strcat(url, buf);
+      http.begin(url);
+
+      int httpCode = http.PUT("");
+
+      if (httpCode > 0) {
+        Serial.println(http.getString());
+      }
+  }
+}
+
+void logOff()
+{
+  pressed = true;
 }
